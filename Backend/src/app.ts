fe551dcbc550eae66134e25203bd566738412111
Augusto Moratti <augus.m0804@gmail.com>
2025-07-28 16:01:55 -0300

    cambio nombre nomas

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,11 +1,17 @@
 import express from 'express'
 import { Request, Response, NextFunction } from 'express'
 import { provinciaRouter } from './provincia/provincia.routes.js'
+import { profesionesRouter } from './profesion/profesion.routes.js'
+import { administradorRouter } from './admin/admin.routes.js'
+
 
 const app = express()
 app.use(express.json())
 
 app.use('/api/provincia', provinciaRouter)
+app.use('/api/profesion', profesionesRouter)
+app.use('/api/admin', administradorRouter)
+
 
 app.use((_req: Request, res: Response, _next: NextFunction) => {
   return res.status(404).send({ message: 'Not found' })
--- Backend/src/profesion/profesion.repository.ts
+++ Backend/src/profesion/profesion.repository.ts
@@ -1,43 +1,43 @@
 import { Repository } from "../shared/repository.js";
 import { Profesion } from "./profesion.entity.js";
 
-const Profesiones: Profesion[] = [
+const profesiones: Profesion[] = [
   new Profesion(
     'Plomero',
     'Encargado de reparar tuberías y sistemas de fontanería',
     true,
     'a02b91bc-3769-4221-beb1-d7a3aeba7dad',
   ),
-];
+]
 
 export class ProfesionRepository implements Repository<Profesion> {
 
   public findAll(): Profesion[] | undefined {
-    return Profesiones;
+    return profesiones;
   }
 
   public findOne(item: { id: string }): Profesion | undefined {
-    return Profesiones.find(prof => prof.codProf === item.id);
+    return profesiones.find(prof => prof.codProf === item.id);
   }
 
   public add(item: Profesion): Profesion | undefined {
-    Profesiones.push(item);
+    profesiones.push(item);
     return item;
   }
 
   public update(item: Profesion): Profesion | undefined {
-  const i = Profesiones.findIndex(prof => prof.codProf === item.codProf);
-  if (i !== -1) {
-    Profesiones[i] = item;
-    return item;
-  }
-  return undefined;
+    const i = profesiones.findIndex(prof => prof.codProf === item.codProf);
+    if (i !== -1) {
+      profesiones[i] = item;
+      return item;
+    }
+    return undefined;
   }
 
   public delete(item: { id: string }): Profesion | undefined {
-    const i = Profesiones.findIndex(prof => prof.codProf === item.id);
+    const i = profesiones.findIndex(prof => prof.codProf === item.id);
     if (i !== -1) {
-      return Profesiones.splice(i, 1)[0];
+      return profesiones.splice(i, 1)[0];
     }
     return undefined;
   }

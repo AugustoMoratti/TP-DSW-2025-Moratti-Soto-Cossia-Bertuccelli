f5720b1b4528661683be9d214317d09348591a08
Lucas <luqui.cossia@gmail.com>
2025-07-30 19:47:10 -0300

    Se cambiaron unos tabs y /d

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -6,8 +6,6 @@ import { administradorRouter } from './admin/admin.routes.js'
 import { localidadesRouter } from './localidad/localidad.routes.js'
 import { usuarioRouter } from './usuario/usuario.routes.js'
 
-
-
 const app = express()
 app.use(express.json())
 
@@ -16,9 +14,6 @@ app.use('/api/profesion', profesionesRouter)
 app.use('/api/admin', administradorRouter)
 app.use('/api/localidad', localidadesRouter)
 app.use('/api/usuario', usuarioRouter)
-
-
-
 
 app.use((_req: Request, res: Response, _next: NextFunction) => {
   return res.status(404).send({ message: 'Not found' })

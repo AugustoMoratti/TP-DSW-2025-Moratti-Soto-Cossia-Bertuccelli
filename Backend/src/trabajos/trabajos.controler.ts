11aa8e01d4a28165b62573198aaa7c9abd5c03d7
ValenBertu <valentinbertuccelli@gmail.com>
2025-09-24 13:15:51 -0300

    Update trabajos.controler.ts

--- Backend/src/trabajos/trabajos.controler.ts
+++ Backend/src/trabajos/trabajos.controler.ts
@@ -20,23 +20,61 @@ function sanitizeTrabajoInput(req: Request, res: Response, next: NextFunction) {
 }
 
 function findAll(req: Request, res: Response) {
-
+  try {
+    const trabajos = await em.find(Trabajo, {})
+    res
+      .status(200)
+      .json({ message: 'found all Trabajos', data: trabajos })
+  } catch (error: any) {
+    res.status(500).json({ message: error.message })
+  }
 }
 
 function findOne(req: Request, res: Response) {
-
+  try {
+    const id = Number.parseInt(req.params.id)
+    const trabajo = await em.findOneOrFail(Trabajo, { id })
+    res
+      .status(200)
+      .json({ message: 'found Trabajo', data: trabajo })
+  } catch (error: any) {
+    res.status(500).json({ message: error.message })
+  }
 }
 
 function add(req: Request, res: Response) {
-
+  try {
+    const trabajo = em.create(Trabajo, req.body)
+    await em.flush()
+    res
+      .status(201)
+      .json({ message: 'Trabajo class created', data: trabajo })
+  } catch (error: any) {
+    res.status(500).json({ message: error.message })
+  }
 }
 
 function update(req: Request, res: Response) {
-
+  try {
+    const id = Number.parseInt(req.params.id)
+    const trabajo = em.getReference(Trabajo, id)
+    em.assign(trabajo, req.body)
+    await em.flush()
+    res.status(200).json({ message: 'Trabajo class updated' })
+  } catch (error: any) {
+    res.status(500).json({ message: error.message })
+  }
 }
 
 function remove(req: Request, res: Response) {
-
+  try {
+    const id = Number.parseInt(req.params.id)
+    const trabajo = em.getReference(Trabajo, id)
+    await em.removeAndFlush(Trabajo)
+    res.status(200).send({ message: 'Trabajo deleted' })
+  } catch (error: any) {
+    res.status(500).json({ message: error.message })
+  }
 }
 
 export { sanitizeTrabajoInput, findAll, findOne, add, update, remove }

1f1d2b14b245e50f28ae4a7ccceeaeb5ac0e418d
Lucas <luqui.cossia@gmail.com>
2025-10-06 18:26:05 -0300

    Pagina de LOGIN presentada, (Hay que crear nuevo archivos y cambiarlo de lugar pero esta hecha)

--- Frontend/conectar/src/App.tsx
+++ Frontend/conectar/src/App.tsx
@@ -2,25 +2,38 @@ import React, { useState } from 'react';
 import './app.css';
 import StandardInput, { Button } from './components/Form';
 import CheckIcon from '@mui/icons-material/Check';
+import { useNavigate } from "react-router-dom"; // Usando react-router
 
 export default function App() {
   const [usuario, setUsuario] = useState("");
   const [clave, setClave] = useState("");
+  const navigate = useNavigate(); 
 
   return (
     <section className="main-bg">
+        <img src="../assets/conect_1.png" alt="Logo" className='logo'/>
       <div className="card">
         <div className="card-header">
-          <span className="card-title">CONECTAR</span>
+          <span className="card-title">INICIE SESIÓN </span>
         </div>
         <div className="card-content">
           <StandardInput label="Usuario" value={usuario} onChange={setUsuario} />
           <StandardInput label="Clave" value={clave} onChange={setClave} type="password" />
-          <Button variant="contained" icon={<CheckIcon />}>
-            Enviar
-          </Button>
+          <div className="card-actions">
+            <button
+              className="cta-link"
+              type="button"
+              onClick={() => navigate("/registro")}
+            >
+              ¿No tiene cuenta? Cree una ahora mismo
+            </button>
+            <Button variant="contained" icon={<CheckIcon />}>
+              Enviar 
+            </Button> 
+          </div>
         </div>
       </div>
     </section>
   );
 }
+//CAMBIAR COLOR DEL BOTON

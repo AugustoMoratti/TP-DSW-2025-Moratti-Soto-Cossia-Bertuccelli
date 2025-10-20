7ea376c89bb7b1131eff91574383e6d22fb58e17
Augusto Moratti <augus.m0804@gmail.com>
2025-10-20 15:19:40 -0300

    Copie y pegue mi perfil en su perfil para empezar a trabajar en algun momento

--- Frontend/conectar/src/main.tsx
+++ Frontend/conectar/src/main.tsx
@@ -7,6 +7,7 @@ import Registro from './pages/register/register.tsx'
 import BusquedaProfesionales from './pages/BusquedaProfesionales/busqProf.tsx'
 import NotFound from './pages/error/notFound.tsx'
 import Perfil from './pages/MiPerfil/perfil.tsx';
+import SuPerfil from './pages/SuPefil/SuPerfil.tsx'
 import InternalServerError from './pages/error/error500.tsx';
 import Dashboard from './pages/Dashboard/dashboard.tsx'
 import Terminos from './pages/terminos/terminos.tsx';
@@ -19,6 +20,7 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Registro />} />
         <Route path="/perfil" element={<Perfil />} />
+        <Route path="/SuPerfil" element={<SuPerfil />} />
         <Route path="/busqProfesionales" element={<BusquedaProfesionales />} />
         <Route path="/*" element={<NotFound />} />
         <Route path="/500" element={<InternalServerError />} />
---
+++ Frontend/conectar/src/pages/SuPefil/SuPerfil.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import ProfileCard from "../../components/ProfileCard/ProfileCard";
+import Header from "../../components/header/header";
+import "./SuPerfil.css";
+import { useNavigate } from "react-router";
+
+
+const SuPerfil: React.FC = () => {
+  const navigate = useNavigate()
+  return (
+    <div className="app-container">
+      <Header bgColor="#ffffff" logoSrc="/assets/conect_2_1.png" >
+        <button className="header-btn" onClick={() => navigate("/")}>Home</button>
+        <button className="header-btn" onClick={() => navigate("/busqProfesionales")}>Buscar Profesionales</button>
+      </Header>
+
+      <main>
+        <div className="profile-wrapper">
+          <ProfileCard />
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default SuPerfil;
+

9e66fe5f8c0b7e6a54859f3e61c100dd81a17091
Augusto Moratti <augus.m0804@gmail.com>
2025-10-21 17:22:26 -0300

    asdas

--- Frontend/conectar/src/components/cardsProfesionales/perfilCard.tsx
+++ Frontend/conectar/src/components/cardsProfesionales/perfilCard.tsx
@@ -1,11 +1,13 @@
 import type { Usuario } from "../../interfaces/usuario";
 import "./perfilCard.css";
+import { useNavigate } from "react-router";
 
 interface Props {
   usuario: Usuario;
 }
 
 export default function PerfilCard({ usuario }: Props) {
+  const navigate = useNavigate();
   return (
     <section className="card-user">
       <div className="user-info">
@@ -28,7 +30,7 @@ export default function PerfilCard({ usuario }: Props) {
       </div>
       <div className="pie-card">
         <p className="resenia">★★★☆☆</p>
-        <button className="ver-perfil-btn">Ver perfil</button>
+        <button className="ver-perfil-btn" onClick={() => navigate('/SuPerfil/:id')}>Ver perfil</button>
       </div>
     </section>
   );

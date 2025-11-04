5bc909c19074b9381139554b5f5c430fd0a90d83
Augusto Moratti <augus.m0804@gmail.com>
2025-11-04 20:56:58 -0300

    apis listas en el frontend, falta mostrar los datos nomas

--- Frontend/conectar/src/pages/trabajo/trabajo.tsx
+++ Frontend/conectar/src/pages/trabajo/trabajo.tsx
@@ -2,16 +2,35 @@ import type { Usuario } from "../../interfaces/usuario.ts";
 import type { Trabajo } from "../../interfaces/trabajo.ts";
 import { useNavigate } from "react-router-dom";;
 import Header from "../../components/header/header";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { fetchMe } from "../../services/auth.services.ts";
 
 export default function Trabajos() {
   const navigate = useNavigate();
   const [trabajosFinalizados, setTrabajosFinalizados] = useState<Trabajo[]>([])
   const [trabajosPendientes, setTrabajosPendientes] = useState<Trabajo[]>([])
 
-  useEffect() => {
-    const res = await fetch("http://localhost:3000/api/trabajo/")
-  }
+
+
+  useEffect(() => {
+    (async () => {
+      const usuario = await fetchMe();
+      const res = await fetch(`http://localhost:3000/api/trabajos/finalizados/${usuario.id}`, { method: "GET" })
+      const data = await res.json()
+      console.log(data.data)
+      setTrabajosFinalizados(data.data)
+    })()
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      const usuario = await fetchMe();
+      const res = await fetch(`http://localhost:3000/api/trabajos/pendientes/${usuario.id}`, { method: "GET" })
+      const data = await res.json()
+      console.log(data.data)
+      setTrabajosPendientes(data.data)
+    })()
+  }, []);
 
   return (
     <div>
@@ -25,7 +44,10 @@ export default function Trabajos() {
       </Header>
 
       <main>
-        <section>
+        <section className="trabajos_pendientes_container">
+
+        </section>
+        <section className="trabajos_finalizados_container">
 
         </section>
       </main>

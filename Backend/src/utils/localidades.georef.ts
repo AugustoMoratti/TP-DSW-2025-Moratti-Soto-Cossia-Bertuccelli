0b59905aef2fb3ab53cb9189caf03b943b045f10
Augusto Moratti <augus.m0804@gmail.com>
2026-01-05 10:48:35 -0300

    codigo que no se utiliza

--- Backend/src/utils/localidades.georef.ts
+++
@@ -1,9 +0,0 @@
-
-export const fetchLocalidadesByProvincia = async (provinciaId: string) => {
-  const res = await fetch(
-    `https://apis.datos.gob.ar/georef/api/localidades?provincia=${provinciaId}`
-  );
-
-  const data = await res.json();
-  return data.localidades;
-}
--- Backend/src/utils/provincias.georef.ts
+++
@@ -1,7 +0,0 @@
-export const fetchProvincias = async () => {
-  const res = await fetch(
-    'https://apis.datos.gob.ar/georef/api/provincias'
-  );
-  const data = await res.json();
-  return data.provincias;
-}

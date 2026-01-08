bb3afdb69ca119ea1eb4b027e817f8cae539edaa
Augusto Moratti <augus.m0804@gmail.com>
2026-01-08 12:22:10 -0300

    Controller de usuario , se arreglo el addProfesiones y se agrego el buen manejo de errores

--- Backend/src/usuario/usuario.controler.ts
+++ Backend/src/usuario/usuario.controler.ts
@@ -7,6 +7,7 @@ import { orm } from '../../DB/orm.js';
 import { RequestContext } from '@mikro-orm/core';
 import { EntityManager } from '@mikro-orm/core';
 import { hashPassword, comparePassword } from '../utils/bcryp.js';
+import { HttpError } from '../types/HttpError.js';
 
 const em = orm.em.fork();
 
@@ -36,7 +37,7 @@ function sanitizeUsuarioInput(req: Request, res: Response, next: NextFunction) {
   next()
 }
 
-async function buscarUsuarios(req: Request, res: Response) {
+async function buscarUsuarios(req: Request, res: Response, next: NextFunction) {
   const em = orm.em.fork();
 
   try {
@@ -57,53 +58,62 @@ async function buscarUsuarios(req: Request, res: Response) {
       populate: ['provincia', 'localidad', 'profesiones', 'trabajos', 'trabajos.resenia'],
       limit: 10
     });
-
-    return res.json({ data: usuarios });
-  } catch (err) {
-    console.error('Error en buscarUsuarios:', err);
-    return res.status(500).json({ message: 'Error al buscar usuarios' });
+    if (usuarios.length === 0) {
+      res.status(200).json({ message: 'No hay usuarios con esas especificaciones', data: usuarios })
+    }
+    return res.status(200).json({ data: usuarios });
+  } catch (error: any) {
+    next(error)
   }
 } //quiero en un futuro poder ordenarlos por resenia
 
-async function findAll(req: Request, res: Response) {
+async function findAll(req: Request, res: Response, next: NextFunction) {
   try {
     const usuarios = await em.find(Usuario, {}, { populate: ['profesiones', 'localidad'] })
+    if (usuarios.length === 0) {
+      res
+        .status(200)
+        .json({ message: 'No se encontraron Usuarios', data: usuarios })
+    }
     res
       .status(200)
-      .json({ message: 'found all Usuarios', data: usuarios })
-  } catch (err) {
-    console.error('Error en findAll:', err);
-    return res.status(500).json({ message: 'Error al buscar todos los usuarios' });
+      .json({ message: 'Usuarios encontrados', data: usuarios })
+  } catch (error) {
+    next(error)
   }
 }
 
-async function findOne(req: Request, res: Response) {
+async function findOne(req: Request, res: Response, next: NextFunction) {
   try {
     const id = req.params.id
-    const usuario = await em.findOneOrFail(Usuario, { id }, { populate: ['profesiones', 'trabajos', 'trabajos.resenia', 'provincia', 'localidad'] })
+    const usuario = await em.findOne(Usuario, { id }, { populate: ['profesiones', 'trabajos', 'trabajos.resenia', 'provincia', 'localidad'] })
+    if (!usuario) {
+      throw new HttpError(404, 'NOT_FOUND', 'No se encontró el Usuario')
+    }
     res
       .status(200)
       .json({ message: 'found Usuario', data: usuario })
-  } catch (err) {
-    console.error('Error en findOne:', err);
-    return res.status(500).json({ message: 'Error al buscar el usuario' });
+  } catch (error: any) {
+    next(error)
   }
 }
 
 
-export async function addProfesiones(req: Request, res: Response) {
-  const em = RequestContext.getEntityManager();
+export async function addProfesiones(req: Request, res: Response, next: NextFunction) {
+  //const em = RequestContext.getEntityManager();
   //ver de cambiar por ( const em = orm.em.fork(); )
+
+  const em = orm.em.fork();
 
   try {
     const userId = req.body.userId as string | number | undefined;
     if (!userId) {
-      return res.status(400).json({ message: "Falta userId" });
+      throw new HttpError(400, 'INVALID_INPUT', 'Falta userId')
     }
 
     const raw = req.body.profesiones;
     if (!Array.isArray(raw)) {
-      return res.status(400).json({ message: "El campo 'profesiones' debe ser un arreglo" });
+      throw new HttpError(400, 'INVALID_INPUT', 'El campo profesiones debe ser un arreglo')
     }
 
     const profesionesName = raw
@@ -112,20 +122,28 @@ export async function addProfesiones(req: Request, res: Response) {
       .filter((p) => p.length > 0)
       .map((p) => p.toLowerCase());
 
-    if (!em) {
+    /*if (!em) {
       return res.status(500).json({ message: 'EntityManager no inicializado' });
-    }
+    }*/
 
-    await em.transactional(async (tem: EntityManager) => {
+
+    const result = await em.transactional<
+      | { type: 'CLEARED' }
+      | { type: 'UPDATED'; profesiones: Profesiones[] }
+    >(async (tem: EntityManager) => {
+
       const usuario = await tem.findOne(Usuario, { id: String(userId) }, { populate: ["profesiones"] });
       if (!usuario) {
-        return res.status(404).json({ message: "Usuario no encontrado" });
+        throw new HttpError(404, 'NOT_FOUND', 'Usuario no encontrado')
+
       }
 
       if (profesionesName.length === 0) {
         usuario.profesiones.set([]); // reemplaza toda la colección
         await tem.flush();
-        return res.status(200).json({ message: "Profesiones eliminadas" });
+        return { type: 'CLEARED' } as const;
+        /*await tem.flush();
+        return res.status(200).json({ message: "Profesiones eliminadas" });*/
       }
 
       const profesionesRef = await tem.find(Profesiones, {
@@ -134,27 +152,43 @@ export async function addProfesiones(req: Request, res: Response) {
       });
 
       if (profesionesRef.length === 0) {
-        return res.status(400).json({ message: "Ninguna profesión válida encontrada" });
+        throw new HttpError(400, 'INVALID_INPUT', 'Ninguna profesión válida encontrada')
       }
 
       usuario.profesiones.set(profesionesRef);
-
       await tem.flush();
-
-      return res.status(200).json({
+      return {
+        type: 'UPDATED',
+        profesiones: profesionesRef
+      } as const;
+      /*return res.status(200).json({
         message: "Profesiones actualizadas",
         total: profesionesRef.length,
         profesiones: profesionesRef.map((p) => ({ id: p.nombreProfesion })),
-      });
+      });*/
     });
+
+    if (result.type === 'CLEARED') {
+      return res.status(200).json({
+        message: 'Profesiones eliminadas'
+      })
+    }
+
+    return res.status(200).json({
+      message: 'Profesiones actualizadas',
+      total: result.profesiones.length,
+      profesiones: result.profesiones.map((p) => ({
+        nombre: p.nombreProfesion,
+      })),
+    });
+
   } catch (error: any) {
-    console.error(error);
-    return res.status(500).json({ message: error?.message ?? "Error interno" });
+    next(error)
   }
 }
 
 
-async function add(req: Request, res: Response) {
+async function add(req: Request, res: Response, next: NextFunction) {
   try {
     //const usuario = em.create(Usuario, req.body)
     const { nombre, apellido, clave, email, descripcion, contacto, horarios, provincia, localidad, profesiones, fechaNac, direccion } = req.body.sanitizedInput
@@ -179,39 +213,39 @@ async function add(req: Request, res: Response) {
     };
 
     if (typeof nombre !== 'string' || nombre === "") {
-      return res.status(400).json({ message: 'El nombre es obligatorio y debe ser un string' })
+      throw new HttpError(400, 'INVALID_INPUT', 'El nombre es obligatorio y debe ser un string')
     };
 
     if (typeof apellido !== 'string' || apellido === "") {
-      return res.status(400).json({ message: 'El apellido es obligatorio y debe ser un string' })
+      throw new HttpError(400, 'INVALID_INPUT', 'El apellido es obligatorio y debe ser un string')
     };
 
     if (typeof clave !== 'string' || clave === "") {
-      return res.status(400).json({ message: 'La clave es obligatoria y debe ser un string' })
+      throw new HttpError(400, 'INVALID_INPUT', 'La clave es obligatoria y debe ser un string')
     };
 
     if (typeof email !== 'string' || email === "") {
-      return res.status(400).json({ message: 'El email es obligatorio y debe ser un string' })
+      throw new HttpError(400, 'INVALID_INPUT', 'El email es obligatorio y debe ser un string')
     };
 
     if (typeof contacto !== 'string') {
-      return res.status(400).json({ message: 'El numero de contacto es obligatorio y debe ser un string' })
+      throw new HttpError(400, 'INVALID_INPUT', 'El numero de contacto es obligatorio y debe ser un string')
     };
 
     if (typeof fechaNac !== 'string' || fechaNac === "") {
-      return res.status(400).json({ message: 'El horario es obligatorio y debe ser un string' })
+      throw new HttpError(400, 'INVALID_INPUT', 'El horario es obligatorio y debe ser un string')
     };
 
     if (typeof horarios !== 'string' || horarios === "") {
-      return res.status(400).json({ message: 'El horario es obligatorio y debe ser un string' })
+      throw new HttpError(400, 'INVALID_INPUT', 'El horario es obligatorio y debe ser un string')
     };
 
     if (descripcion && typeof descripcion !== 'string') {
-      return res.status(400).json({ message: 'La descripcion debe ser un string' })
+      throw new HttpError(400, 'INVALID_INPUT', 'La descripcion debe ser un string')
     };
 
     if (direccion && typeof direccion !== 'string') {
-      return res.status(400).json({ message: 'La direccion debe ser un string' })
+      throw new HttpError(400, 'INVALID_INPUT', 'La direccion debe ser un string')
     };
 
     const usuario = new Usuario()
@@ -238,18 +272,18 @@ async function add(req: Request, res: Response) {
     await em.flush()
     res
       .status(201)
-      .json({ message: 'Usuario class created', data: usuario })
+      .json({ message: 'Usuario creado', data: usuario })
   } catch (error: any) {
-    res.status(500).json({ message: error.message })
+    next(error)
   }
 }
 
-async function deleteProfesion(req: Request, res: Response) {
+async function deleteProfesion(req: Request, res: Response, next: NextFunction) {
   try {
     const id = req.params.id;
     const usuario = await em.findOne(Usuario, { id }, { populate: ['profesiones'] });
     if (!usuario) {
-      return res.status(404).json({ message: "Usuario no encontrado" });
+      throw new HttpError(404, 'NOT_FOUND', 'Usuario no encontrado')
     }
 
     const profesionesName: string[] = Array.isArray(req.body.profesiones)
@@ -277,19 +311,18 @@ async function deleteProfesion(req: Request, res: Response) {
     await em.flush();
     res.status(200).json({ message: "Usuario actualizado correctamente", data: usuario });
   } catch (error: any) {
-    console.error(error);
-    res.status(500).json({ message: error.message })
+    next(error)
   }
 }
 
-async function update(req: Request, res: Response) {
+async function update(req: Request, res: Response, next: NextFunction) {
   try {
     console.log(req.body);
 
     const id = req.params.id;
     const usuario = await em.findOne(Usuario, { id }, { populate: ['profesiones'] });
     if (!usuario) {
-      return res.status(404).json({ message: "Usuario no encontrado" });
+      throw new HttpError(404, 'NOT_FOUND', 'Usuario no encontrado')
     }
 
     const { nombre, apellido, fechaNac, provincia, localidad, clave, email, descripcion, contacto, horarios, direccion, habilidades } = req.body;
@@ -339,20 +372,19 @@ async function update(req: Request, res: Response) {
     await em.flush();
     res.status(200).json({ message: "Usuario actualizado correctamente", data: usuario });
   } catch (error: any) {
-    console.error(error);
-    res.status(500).json({ message: error.message });
+    next(error)
   }
 }
 
 
-async function remove(req: Request, res: Response) {
+async function remove(req: Request, res: Response, next: NextFunction) {
   try {
     const id = req.params.id
     const usuario = em.getReference(Usuario, id)
     await em.removeAndFlush(usuario)
-    res.status(200).send({ message: 'Usuario deleted' })
+    res.status(200).send({ message: 'Usuario eliminado' })
   } catch (error: any) {
-    res.status(500).json({ message: error.message })
+    next(error)
   }
 }
 

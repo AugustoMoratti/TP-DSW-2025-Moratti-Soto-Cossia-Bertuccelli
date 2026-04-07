e7a2bed69b779db9329784dbb54ffb87f0d9fb75
LucasCossia <luqui.cossia@gmail.com>
2026-04-07 18:42:20 -0300

    Recuperar contraseña styles

--- Frontend/conectar/src/pages/Password/OlvidarP.tsx
+++ Frontend/conectar/src/pages/Password/OlvidarP.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import "./OlvidarCambiarP.css";
 import { useNavigate } from "react-router-dom";
+import FastRewindIcon from '@mui/icons-material/FastRewind';
+import { animate } from "animejs/animation";
 
 export default function ForgotPassword() {
 
@@ -18,13 +20,33 @@ export default function ForgotPassword() {
     alert("Si el mail existe, se envió el correo");
   };
 
+    const handleEnter = () => {
+      animate(".logo1", {
+        scale: 1.15,
+        duration: 300,
+        ease: "out(3)",
+      });
+    };
+
+    const handleLeave = () => {
+      animate(".logo1", {
+        scale: 1,
+        duration: 300,
+        ease: "out(3)",
+      });
+    };
+
   return (
     <div className="main-bgg">
-      <img src="../assets/conect_1.png" alt="Logo" style={{ height: '150px', cursor: 'pointer' }} onClick={() => Navigate("/")} />
+      <img src="../assets/conect_1.png" className="logo1" alt="Logo" style={{ height: '150px', cursor: 'pointer' }} onClick={() => Navigate("/")} onMouseEnter={handleEnter} onMouseLeave={handleLeave}/>
       <div className="cardd">
         <div className="card-headerr">
-          <h2 className="card-titlee"> RECUPERAR CONTRASEÑA</h2>
+          <h2 className="card-titlee">RECUPERAR CONTRASEÑA</h2>
         </div>
+
+        <p className="info-texto">
+          Te enviaremos un link para cambiar tu contraseña
+        </p>
 
         <div className="card-contentt">
           <input
@@ -35,13 +57,15 @@ export default function ForgotPassword() {
             onChange={e => setEmail(e.target.value)}
           />
 
-          <button className="btn-primaryo" onClick={send}>
-            Enviar link
-          </button>
+          <div>
+            <button onClick={() => Navigate("/-1")} className="btn-secondaryy" title="Volver atrás">
+              <FastRewindIcon/>
+            </button>
+            <button className="btn-primaryo" onClick={send}>
+              Enviar link
+            </button>
+          </div>
 
-          <p className="info-texto">
-            Te enviaremos un link para cambiar tu contraseña
-          </p>
         </div>
       </div>
     </div>
